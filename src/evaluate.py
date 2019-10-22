backup fixing store transition for macro. Minor changes, added gradient clipping / smooth l1 loss

src/evaluate.py
@@ -1,3 +1,4 @@
+import time
 import gym
 import Custom_Env.DimensionalGrid
 import gym_super_mario_bros
@@ -6,7 +7,6 @@ from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from rl.ehdqn import EHDQN
 import logging
 import argparse
-import logger as log
 import numpy as np
 from gym.wrappers import AtariPreprocessing
 from rl.frame_stack import FrameStack
@@ -22,6 +22,8 @@ def boolean_string(s):
 
 
 parser.add_argument('--env', default='DimGrid-v0', type=str, help='Environment name')
+parser.add_argument('--ckpt', default='./', type=str, help='Ckpt path')
+parser.add_argument('--step', default=-1, type=int, help='Ckpt step value')
 parser.add_argument('--episodes', default=1_000_000, type=int, help='number of episodes')
 parser.add_argument('--gamma', default=0.9, type=float, help='Discount reward factor')
 parser.add_argument('--gamma_macro', default=0.9, type=float, help='Discount reward factor for macro policy')
@@ -32,18 +34,18 @@ parser.add_argument('--max_time', default=4, type=int, help='Number of steps per
 parser.add_argument('--n_subpolicy', default=2, type=int, help='Number of sub policies')
 parser.add_argument('--lr', default=1e-3, type=float, help='Learning rate for agent training')
 parser.add_argument('--eps', default=0.5, type=float, help='Chance of taking random action')
-parser.add_argument('--eps_decay', default=4e-5, type=float, help='Decay for macro eps')
+parser.add_argument('--eps_decay', default=1e-4, type=float, help='Decay for macro eps')
 parser.add_argument('--eps_sub', default=0.5, type=float, help='Chance of taking random action for sub policy')
-parser.add_argument('--eps_sub_decay', default=4e-5, type=float, help='Decay for sub policy eps')
+parser.add_argument('--eps_sub_decay', default=1e-4, type=float, help='Decay for sub policy eps')
 parser.add_argument('--bs', default=32, type=int, help='Batch size')
 parser.add_argument('--train_interval', default=5, type=int, help='Steps of env before training')
 parser.add_argument('--train_steps', default=1, type=int, help='Steps of training')
 parser.add_argument('--target_int', default=100, type=int, help='Steps of training to update target network in dueling arch.')
-parser.add_argument('--max_memory', default=100_000, type=int, help='Max memory')
+parser.add_argument('--max_memory', default=50_000, type=int, help='Max memory')
 parser.add_argument('--size', default=20, type=int, help='Size for DimGrid environment')
 parser.add_argument('--tau', default=0.01, type=float, help='Weight for agent loss')
 parser.add_argument('--beta', default=0.2, type=float, help='Weight for fwd vs inv icm loss')
-parser.add_argument('--reward_rescale', default=0., type=float, help='Reward rescaling: 0 -> Id, 1 -> sign(R), 2 -> [-1;1], float -> Rew * mult')
+parser.add_argument('--reward_rescale', default=0., type=float, help='Reward rescaling: 0 -> Id, 1 -> sign(R), float -> Rew * mult')
 
 
 if __name__ == '__main__':
@@ -56,10 +58,10 @@ if __name__ == '__main__':
         env = gym.make(args.env)
         env = env = JoypadSpace(env, SIMPLE_MOVEMENT)
         env = RepeatAction(env, nskip=6)
-        #env = TimeLimitMario(env, time=300) # 400 - time = total_seconds
+        env = TimeLimitMario(env, time=300) # 400 - time = total_seconds
         #env = LifeLimitMario(env)
-        env = ResizeState(env, res=(48, 48), gray=True)
-        env = FixGrayScale(env)
+        env = ResizeState(env, res=(48, 48), gray=False)
+        #env = FixGrayScale(env)
         env = FrameStack(env, num_stack=4)
         env = ChannelsConcat(env)
 
@@ -90,65 +92,26 @@ if __name__ == '__main__':
                 train_steps=args.train_steps,
                 max_memory=args.max_memory,
                 conv=conv,
-                gamma_macro=args.gamma_macro,
                 reward_rescale=args.reward_rescale,
-                logger=log.TB_LOGGER
+                gamma_macro=args.gamma_macro
                 )
 
-    train_steps = 0
-    tot_succ = 0
-    episode_duration = 0
-    for i in range(args.episodes):
-        if i % 5 == 0:
-            logging.info('Episode: %s' % i)
+    # Load model
+    dqn.load(args.ckpt, i=args.step)
 
-        log.TB_LOGGER.step += 1
+    while True:
+        tot_reward = 0
         obs = env.reset()
-
         while True:
-            action = dqn.act(obs[np.newaxis])
+            action = dqn.act(obs[np.newaxis], deterministic=True)
             obs_new, r, is_terminal, _ = env.step(action)
 
-            tot_succ += r
-            dqn.store_transition(obs, obs_new, action, r, is_terminal)
-
-            train_steps += 1
-            if train_steps % args.train_interval == 0 and train_steps > 0:
-                train_steps = 0
-                dqn.update()
-
+            env.render()
+            time.sleep(1e-2)
+            tot_reward += r
             obs = obs_new
-            episode_duration += 1
+
             if is_terminal:
-                log.TB_LOGGER.log_scalar(tag='Episode Duration', value=episode_duration)
-                episode_duration = 0
                 break
 
-        episodes_per_epoch = 50
-        if i % episodes_per_epoch == 0 and i > 0:
-            log.TB_LOGGER.log_scalar(tag='Train Reward:', value=tot_succ / episodes_per_epoch)
-            tot_succ = 0
-
-        if i % 100 == 0:
-            n_eval_episodes = 1
-            tot_reward = 0
-            for _ in range(n_eval_episodes):
-                obs = env.reset()
-                while True:
-                    action = dqn.act(obs[np.newaxis], deterministic=True)
-                    obs_new, r, is_terminal, _ = env.step(action)
-
-                    #env.render()
-                    tot_reward += r
-                    obs = obs_new
-
-                    if is_terminal:
-                        break
-
-            eval_succ = tot_reward / n_eval_episodes
-            logging.info('Mean Reward: %s' % (eval_succ))
-            log.TB_LOGGER.log_scalar(tag='Eval Reward', value=eval_succ)
-
-        # Save ckpt
-        if i % 100 == 0 and i > 0:
-            dqn.save(i)
+        logging.info('Reward: %s' % (tot_reward))

src/rl/custom_wrappers.py
@@ -54,11 +54,10 @@ class RepeatAction(gym.Wrapper):
         return observation, reward, done, info
 
 class ResizeState(gym.Wrapper):
-    def __init__(self, env, res=(64, 64), gray=False, norm=True):
+    def __init__(self, env, res=(64, 64), gray=False):
         super(ResizeState, self).__init__(env)
         self.res = res
         self.gray = gray
-        self.norm = norm
 
         low = self.observation_space.low[:res[0], :res[1], :]
         high = self.observation_space.high[:res[0], :res[1], :]
@@ -72,8 +71,6 @@ class ResizeState(gym.Wrapper):
         observation = cv2.resize(observation, self.res)
         if self.gray:
             observation = cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
-        if self.norm:
-            observation = observation.astype(np.float) / 255
         return observation, reward, done, info
 
     def reset(self, **kwargs):
@@ -81,8 +78,6 @@ class ResizeState(gym.Wrapper):
         observation = cv2.resize(observation, self.res)
         if self.gray:
             observation = cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
-        if self.norm:
-            observation = observation.astype(np.float) / 255
         return observation
 
 class LifeLimitMario(gym.Wrapper):

src/rl/ehdqn.py
@@ -2,15 +2,16 @@ import numpy as np
 import torch
 from src.rl.model import DDQN_Model, ICM_Model
 from src.rl.memory import Memory
-from torch.nn.functional import mse_loss, cross_entropy
+from torch.nn.functional import mse_loss, cross_entropy, smooth_l1_loss
 import settings as sett
 import itertools
 import os
 
 
 class EHDQN:
-    def __init__(self, state_dim, embed_state_dim, tau, action_dim, gamma, n_subpolicy, max_time, hidd_ch, lam,
-                 lr, eps, eps_decay, eps_sub, eps_sub_decay, beta, bs, target_interval, train_steps, max_memory, conv, gamma_macro, logger=None):
+    def __init__(self, state_dim, embed_state_dim, tau, action_dim, gamma, n_subpolicy, max_time, hidd_ch, lam, lr, eps,
+                 eps_decay, eps_sub, eps_sub_decay, beta, bs, target_interval, train_steps, max_memory, conv, gamma_macro,
+                 reward_rescale, norm_input=True, logger=None):
         """
         :param state_dim: Shape of the state
         :param int embed_state_dim: Length of the embed state
@@ -62,6 +63,8 @@ class EHDQN:
         self.macro_state = None
         self.max_time = max_time
         self.train_steps = train_steps
+        self.reward_rescale = reward_rescale
+        self.norm_input = norm_input
         self.curr_time = 0
         self.macro_reward = 0
         self.target_count = np.zeros((self.n_subpolicy,), dtype=np.int)
@@ -94,29 +97,56 @@ class EHDQN:
         for sub in range(self.n_subpolicy):
             torch.save(self.policy[sub].state_dict(), os.path.join(sett.SAVEPATH, 'Sub_%s_%s.pth' % (sub, i)))
 
+    def load(self, path, i):
+        self.macro.load_state_dict(torch.load(os.path.join(path, 'Macro_%s.pth' % i), map_location=sett.device))
+        for sub in range(self.n_subpolicy):
+            self.policy[sub].load_state_dict(torch.load(os.path.join(path, 'Sub_%s_%s.pth' % (sub, i)), map_location=sett.device))
+
     def act(self, obs, deterministic=False):
         x = torch.from_numpy(obs).float().to(sett.device)
+        if self.norm_input:
+            x /= 255
+
         if self.selected_policy is None or self.curr_time == self.max_time:
-            self.selected_policy = self.pick_policy(x)
+            self.selected_policy = self.pick_policy(x, deterministic=deterministic)
             self.counter_macro[self.selected_policy] += 1
         self.curr_time += 1
-        eps = self.eps_sub if not deterministic else 0
+
+        eps = self.eps_sub if not deterministic else 0.025
         action = self.policy[self.selected_policy].act(x, eps=eps)
         return action
 
-    def pick_policy(self, obs):
-        self.macro_state = obs.cpu().numpy()[0]
-        policy = self.macro.act(obs, eps=self.eps)
+    def pick_policy(self, obs, deterministic=False):
+        if not deterministic:
+            self.macro_state = obs.cpu().numpy()[0]
+
+        if self.norm_input:
+            obs = obs.float() / 255
+
+        eps = self.eps if not deterministic else 0
+        cat = True if not deterministic else False
+        policy = self.macro.act(obs, eps=eps, categorical=cat)
         self.curr_time = 0
         return policy
 
     def store_transition(self, s, s1, a, reward, is_terminal):
+        # Rescale reward if a scaling is provided
+        if self.reward_rescale != 0:
+            if self.reward_rescale == 1:
+                reward = np.sign(reward)
+            elif self.reward_rescale == 2:
+                reward = max(-1, min(1, reward))
+            else:
+                reward *= self.reward_rescale
+
         # Store sub policy experience
         self.memory[self.selected_policy].store_transition(s, s1, a, reward, is_terminal)
         self.macro_reward += reward
 
         # Store macro experience
         if self.curr_time == self.max_time or is_terminal:
+            self.macro_reward /= self.max_time
+            print(hash(str(self.macro_state)), hash(str(s1)), is_terminal, self.curr_time == self.max_time)
             self.macro_memory.store_transition(self.macro_state, s1, self.selected_policy, self.macro_reward, is_terminal)
             self.macro_reward = 0
             if is_terminal:
@@ -132,7 +162,7 @@ class EHDQN:
         # First train each sub policy
         for i in range(self.n_subpolicy):
             memory = self.memory[i]
-            if len(memory.state) < self.bs:
+            if len(memory.state) < self.bs * 10:
                 continue
 
             policy = self.policy[i]
@@ -141,6 +171,10 @@ class EHDQN:
             policy_opt = self.policy_opt[i]
 
             state, new_state, action, reward, is_terminal = memory.sample(self.bs)
+            if self.norm_input:
+                state = np.array(state, dtype=np.float) / 255
+                new_state = np.array(new_state, dtype=np.float) / 255
+
             state = torch.tensor(state, dtype=torch.float).detach().to(sett.device)
             new_state = torch.tensor(new_state, dtype=torch.float).detach().to(sett.device)
             action = torch.tensor(action).detach().to(sett.device)
@@ -155,7 +189,7 @@ class EHDQN:
             q = policy(state)[torch.arange(self.bs), action]
             max_action = torch.argmax(policy(new_state), dim=1)
             y = reward + self.gamma * target(new_state)[torch.arange(self.bs), max_action] * is_terminal
-            policy_loss = mse_loss(input=q, target=y.detach())
+            policy_loss = smooth_l1_loss(input=q, target=y.detach())
 
             # ICM Loss
             phi_hat = icm(state, action)
@@ -168,6 +202,8 @@ class EHDQN:
             loss = self.tau * policy_loss + (1 - self.beta) * inv_loss + self.beta * fwd_loss
             policy_opt.zero_grad()
             loss.backward()
+            for param in policy.parameters():
+                param.grad.data.clamp(-1, 1)
             policy_opt.step()
 
             self.target_count[i] += 1
@@ -186,13 +222,17 @@ class EHDQN:
         self.eps_sub = self.eps_sub * self.eps_sub_decay
 
         # Train Macro policy
-        if len(self.macro_memory.state) < self.bs:
+        if len(self.macro_memory.state) < self.bs * 10:
             return
 
         # Reduce eps
         self.eps = self.eps * self.eps_decay
 
         state, new_state, action, reward, is_terminal = self.macro_memory.sample(self.bs)
+        if self.norm_input:
+            state = np.array(state, dtype=np.float) / 255
+            new_state = np.array(new_state, dtype=np.float) / 255
+
         state = torch.tensor(state, dtype=torch.float).detach().to(sett.device)
         new_state = torch.tensor(new_state, dtype=torch.float).detach().to(sett.device)
         action = torch.tensor(action).detach().to(sett.device)
@@ -202,10 +242,12 @@ class EHDQN:
         q = self.macro(state)[torch.arange(self.bs), action]
         max_action = torch.argmax(self.macro(new_state), dim=1)
         y = reward + self.gamma_macro * self.macro_target(new_state)[torch.arange(self.bs), max_action] * is_terminal
-        loss = mse_loss(input=q, target=y.detach())
+        loss = smooth_l1_loss(input=q, target=y.detach())
 
         self.macro_opt.zero_grad()
         loss.backward()
+        for param in self.macro.parameters():
+            param.grad.data.clamp(-1, 1)
         self.macro_opt.step()
 
         self.macro_count += 1

src/rl/memory.py
@@ -33,9 +33,11 @@ class Memory:
 
     def sample(self, bs):
         idx = np.random.randint(len(self.state), size=bs)
-        state = np.array(self.state)[idx]
-        new_state = np.array(self.new_state)[idx]
-        action = np.array(self.action)[idx]
-        reward = np.array(self.reward)[idx]
-        is_terminal = 1 - np.array(self.is_terminal, dtype=np.int)[idx]
+        state, new_state, action, reward, is_terminal = [], [], [], [], []
+        for i in idx:
+            state.append(self.state[i])
+            new_state.append(self.new_state[i])
+            action.append(self.action[i])
+            reward.append(self.reward[i])
+            is_terminal.append(1 - int(self.is_terminal[i]))
         return state, new_state, action, reward, is_terminal

src/rl/model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
+from torch.distributions import Categorical
 
 
 class DDQN_Model(nn.Module):
@@ -12,9 +13,9 @@ class DDQN_Model(nn.Module):
 
         if conv:
             self.features = nn.Sequential(
-                nn.Conv2d(state_size[0], 32, kernel_size=3, stride=2, padding=1),
+                nn.Conv2d(state_size[0], 64, kernel_size=3, stride=2, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+                nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
                 nn.ReLU(),
             )
         else:
@@ -42,10 +43,14 @@ class DDQN_Model(nn.Module):
         value = self.value(x)
         return value + (adv - adv.mean(-1, keepdim=True))
 
-    def act(self, state, eps):
+    def act(self, state, eps, categorical=False):
         if np.random.random() > eps:
-            q = self.forward(state)
-            action = torch.argmax(q, dim=-1).cpu().data.numpy()
+            if categorical:
+                q = self.forward(state)
+                action = Categorical(logits=q).sample().cpu().data.numpy()
+            else:
+                q = self.forward(state)
+                action = torch.argmax(q, dim=-1).cpu().data.numpy()
         else:
             action = np.random.randint(self.action_size, size=1 if len(state.shape) == 1 else state.shape[0])
         return action.item()
@@ -72,11 +77,11 @@ class ICM_Model(nn.Module):
         # Projection
         if conv:
             self.phi = nn.Sequential(
-                nn.Conv2d(self.state_size[0], 32, kernel_size=3, stride=2, padding=1),
+                nn.Conv2d(self.state_size[0], 64, kernel_size=3, stride=2, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+                nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+                nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
                 nn.ReLU()
             )
         else: